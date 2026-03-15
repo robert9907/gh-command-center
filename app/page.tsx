ccbecead0ccbdd3b94024c051fa5cbf8656b3ecb
Rob Simm <robert@generationhealth.me>
2026-03-15 19:28:05 +0000

    Fix page loading - serve index.html content directly

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import { readFileSync } from 'fs';
+import { join } from 'path';
+
 export default function Home() {
-  return null;
+  const html = readFileSync(join(process.cwd(), 'public', 'index.html'), 'utf8');
+  return (
+    <div dangerouslySetInnerHTML={{ __html: html }} />
+  );
 }
